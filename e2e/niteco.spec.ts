d5b1a81a2c04c09a8a155c78470e4fb754b6c70c
Tron Ho <hhtron@gmail.com>
2025-03-29 16:58:51 +0700

    Commit on Saturday

--- e2e/niteco.spec.ts
+++ e2e/niteco.spec.ts
@@ -4,4 +4,10 @@ test('Niteco Site', async ({ page }) => {
   
     // Expect a title "to contain" a substring.
     await expect(page).toHaveTitle(/The World/);
+
+    //Try to click AboutUs menu
+    await page.locator('//a[@class='font-gilroy--semibold link item'][normalize-space()='Our Work']').click();
+
+    //Verify navigating to About Us page
+    await expect(page).toHaveTitle(/Out work/);
   });
