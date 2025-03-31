1d11d0743e0987704a2ceca45a9c3dcfaa228272
Huy Tron <35919807+tronevil@users.noreply.github.com>
2025-03-31 14:47:03 +0700

    add timeout to config file = 120s

--- e2e/niteco.spec.ts
+++ e2e/niteco.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 test('Niteco Site', async ({ page }) => {
-    await page.goto('https://niteco.com/');
+    await page.goto('https://niteco.com');
   
     // Expect a title "to contain" a substring.
     await expect(page).toHaveTitle(/The World/);
