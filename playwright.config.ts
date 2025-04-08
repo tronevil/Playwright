0b390ba133d53382598b73029803fd93d3d41083
Huy Tron <35919807+tronevil@users.noreply.github.com>
2025-04-08 15:53:09 +0700

    add junit report in playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -48,7 +48,7 @@ export default defineConfig({
         } 
 ],
   
-  ['html']],
+  ['html'],['junit', {outputFile: 'results.xml'}]],
   /* Timeout*/
    timeout: 120_000,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -57,6 +57,7 @@ export default defineConfig({
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    
     trace: 'on-first-retry',
   },
 
